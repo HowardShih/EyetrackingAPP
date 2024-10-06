aa4667926d4aa966aea0b6e9b70fbc9fd29c40d8
HowardShih <howardsh0429@gmail.com>
2024-10-07 01:29:37 +0800

    Initial Commit

---
+++ EyetrackingAPP/EyetrackingAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyetrackingAPPApp.swift
+//  EyetrackingAPP
+//
+//  Created by 史承翰 on 2024/10/7.
+//
+
+import SwiftUI
+
+@main
+struct EyetrackingAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EyetrackingAPPTests/EyetrackingAPPTests.swift
@@ -0,0 +1,17 @@
+//
+//  EyetrackingAPPTests.swift
+//  EyetrackingAPPTests
+//
+//  Created by 史承翰 on 2024/10/7.
+//
+
+import Testing
+@testable import EyetrackingAPP
+
+struct EyetrackingAPPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
